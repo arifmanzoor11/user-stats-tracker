e9cb203bf6048c7079ea55a5b3d4dde4048a3836
arifmanzoor11 <arifmanzoor11@gmail.com>
2025-04-16 21:01:45 +0530

    impove tracker

--- assets/js/tracker.js
+++ assets/js/tracker.js
@@ -1,8 +1,8 @@
-
-
 jQuery(document).ready(function($) {
     let startTime = Date.now();
     let isRequestSent = false; // Flag to prevent duplicate requests
+    let activityTimer;
+    const ACTIVITY_INTERVAL = 30000; // 30 seconds
 
     // Check if the current page matches any of the enabled post types
     let enabledPostTypes = uat_ajax.enabled_post_types || [];
@@ -33,40 +33,21 @@ jQuery(document).ready(function($) {
 
         isRequestSent = true; // Set the flag to true after sending the request
     });
+
+    function trackUserActivity() {
+        clearTimeout(activityTimer);
+        
+        const data = {
+            action: 'ust_track_activity',
+            page_url: window.location.href,
+            scroll_depth: calculateScrollDepth(),
+            active_time: getActiveTime()
+        };
+        
+        $.post(uat_ajax.ajax_url, data);
+        
+        activityTimer = setTimeout(trackUserActivity, ACTIVITY_INTERVAL);
+    }
+
+    document.addEventListener('DOMContentLoaded', trackUserActivity);
 });
-
-
-
-// jQuery(document).ready(function ($) {
-//     let startTime = Date.now();
-
-//     // Function to send data to the server
-//     function sendUserStats(data) {
-//         $.post(ustData.ajaxUrl, data)
-//             .done(function (response) {
-//                 if (response.success) {
-//                     console.log('Stats sent successfully:', response.data.message);
-//                 } else {
-//                     console.error('Failed to send stats:', response.data.message);
-//                 }
-//             })
-//             .fail(function (error) {
-//                 console.error('AJAX request failed:', error);
-//             });
-//     }
-
-//     // Send data on page unload
-//     $(window).on('beforeunload', function () {
-//         const endTime = Date.now();
-//         const timeSpent = Math.round((endTime - startTime) / 1000);
-
-//         const data = {
-//             action: 'ust_save_user_stats',
-//             page_url: window.location.href,
-//             time_spent: timeSpent,
-//             navigation_type: 'exit',
-//         };
-
-//         sendUserStats(data);
-//     });
-// });
