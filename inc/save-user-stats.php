ccf171cfe69c0bffe8148227c0b90e6211078b0e
arifmanzoor11 <arifmanzoor11@gmail.com>
2025-06-19 13:54:31 +0530

    added post title post url and email with selized data

--- inc/save-user-stats.php
+++ inc/save-user-stats.php
@@ -1,5 +1,6 @@
 <?php
-function uat_log_activity() {
+function uat_log_activity()
+{
 
     if (!isset($_POST['user_id'], $_POST['duration'], $_POST['navigation_type'], $_POST['date_time'], $_POST['is_single'])) {
         wp_send_json_error('Invalid data');
@@ -13,20 +14,45 @@ function uat_log_activity() {
     global $wpdb;
     $table_name = $wpdb->prefix . 'user_activity';
 
-    $user_id          = intval($_POST['user_id']);
-    $page_id          = intval($_POST['page_id']); // Use page_id instead of page URL
-    $navigation_type  = sanitize_text_field($_POST['navigation_type']);
-    $duration         = floatval($_POST['duration']);
-    $date_time        = sanitize_text_field($_POST['date_time']);
+    $page_url = esc_url_raw($_POST['page_url']);
+    $user_id = intval($_POST['user_id']);
+    $page_id = intval($_POST['page_id']); // Use page_id instead of page URL
+    $navigation_type = sanitize_text_field($_POST['navigation_type']);
+    $duration = floatval($_POST['duration']);
+    $date_time = sanitize_text_field($_POST['date_time']);
+
+    $page_title = get_the_title($page_id);
+    $Agent_Data = get_post_meta($page_id, 'Agent_Data', true);
+
+    $email_values = []; // Collect email(s)
+        if ($Agent_Data) {
+            $data = is_serialized($Agent_Data) ? unserialize($Agent_Data) : $Agent_Data;
+
+            if (is_array($data)) {
+                foreach ($data as $key) {
+                    if (isset($key['Email'])) {
+                      $email_values[] = $key['Email'];
+                    }
+                }
+            }
+        }
+
+    // Prepare serialized data with email(s)
+    $serialize_data = serialize(array(
+        'url'    => $page_url,
+        'title'  => $page_title,
+        'email'  => $email_values,
+    ));
 
     $wpdb->insert($table_name, [
-        'user_id'         => $user_id,
-        'page'            => $page_id, // Save page ID here
+        'user_id' => $user_id,
+        'page' => $serialize_data, // fallback if ID not usable
         'navigation_type' => $navigation_type,
-        'duration'        => $duration,
-        'timestamp'       => current_time('mysql'),
-        'date_time'       => $date_time,
+        'duration' => $duration,
+        'timestamp' => current_time('mysql'),
+        'date_time' => $date_time,
     ]);
+
 
     wp_send_json_success('Activity logged');
 }
